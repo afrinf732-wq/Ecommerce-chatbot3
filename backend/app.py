ae7f2a0e4518fba64bf8cbc23b8cbd24206372d3
afrinf732-wq <afrinf732@gmail.com>
2026-01-22 10:30:01 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,17 @@
+from flask import Flask, request, jsonify
+from chatbot import get_response
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Ecommerce Chatbot Backend is running"
+
+@app.route("/chat", methods=["POST"])
+def chat():
+    user_message = request.json.get("message")
+    response = get_response(user_message)
+    return jsonify({"reply": response})
+
+if __name__ == "__main__":
+    app.run(debug=True)
